e826f33ba8aca4a69ad998a54414abed17926379
Cody Koscielski <codykoscielski@gmail.com>
2022-10-16 09:09:01 -0400

    Refactored to create MVC design pattern

--- NatoursAPI/app.js
+++ NatoursAPI/app.js
@@ -1,149 +1,19 @@
-const fs = require('fs')
 const express = require('express')
 const morgan = require('morgan')
 
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
+
 const app = express()
 app.use(express.json())
-
 app.use(morgan('dev'))
 
-app.use((req, res, next) => {
-    console.log(`hello from the middlewares, bitches`)
-    next()
-})
-
-//Reading the json file to get all tours
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-
-const getAllTours = (req,res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
-}
-
-const createTour = (req, res) => {
-    // console.log(req.body)
-    //Create a new ID based on the last Id that is in the json file
-    const newId = tours[tours.length - 1].id + 1
-    const newTour = Object.assign({id: newId}, req.body)
-
-    tours.push(newTour)
-
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        })
-    })
-}
-
-const getTour = (req,res) => {
-    //This will convert the string into a number
-    const id = req.params.id * 1
-
-    if(id > tours.length) {
-        return res.status(404).json({
-            status: 'Failed',
-            message: 'invalided ID'
-        })
-    }
-
-    const tour = tours.find(el => el.id === id)
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    })
-}
-
-const updateTour = (req, res) => {
-    
-    if(req.params.id * 1 > tours.length) {
-        return res.status(400).json({
-            status: 'failed',
-            message: 'Invalid ID'
-        })
-    }
-    
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: 'Tour updated'
-        }
-    })
-}
-
-const getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet setup'
-    })
-}
-
-const getUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet setup'
-    })
-}
-const createUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet setup'
-    })
-}
-
-const updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet setup'
-    })
-}
-
-const deleteUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet setup'
-    })
-}
-
-
-const tourRouter = express.Router()
-const userRouter = express.Router()
-
-// *tour routes
-tourRouter.route('/')
-    .get(getAllTours)
-    .post(createTour)
-
-tourRouter.route('/:id')
-    .get(getTour)
-    .patch(updateTour)
-
-// *users routes
-userRouter.route('/')
-    .get(getAllUsers)
-    .post(createUser)
-
-userRouter.route('/:id')
-    .get(getUser)
-    .patch(updateUser)
-    .delete(deleteUser)
-
+//Routes
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
-//Port that the server will run on
+//Start the server
 const port = 3000
 app.listen(port, () => {
     console.log(`listening on port ${port}...`)
 })
-
---
+++ NatoursAPI/controllers/userController.js
@@ -0,0 +1,33 @@
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet setup'
+    })
+}
+
+exports.getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet setup'
+    })
+}
+exports.createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet setup'
+    })
+}
+
+exports.updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet setup'
+    })
+}
+
+exports.deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet setup'
+    })
+}
